0c69a39b01de81796ea5f81907cf4086cce1aaea
HeeHeejun <gmlwns123924@gmail.com>
2022-11-13 00:28:33 +0900

    Merge branch 'main' of https://github.com/Leehowook/GDSC6

--- app/src/main/java/com/example/gdsc6/MainActivity.kt
+++ app/src/main/java/com/example/gdsc6/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.gdsc6
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineStart
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
--- app/src/main/java/com/example/gdsc6/page2.kt
+++ app/src/main/java/com/example/gdsc6/page2.kt
@@ -1,4 +1,15 @@
 package com.example.gdsc6
 
-class page2 {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class page2 : LockAppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.page2)
+
+    }
+
 }
